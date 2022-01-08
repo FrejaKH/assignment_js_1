09668ca09a7ec3c94d6265cee96faf48ced47cfd
Freja <freja.hyldgaard@gmail.com>
2022-01-08 16:23:15 +0100

    small changes

--- js/js201.js
+++ js/js201.js
@@ -3,3 +3,11 @@ let chosenYear = prompt('Write in a year');
 console.log('Is the year ' + chosenYear + ' a leap year?');
 
 console.log((chosenYear % 4) === 0);
+
+
+
+/* Løsning
+
+(year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
+
+*/
